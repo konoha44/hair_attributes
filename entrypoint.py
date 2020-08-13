ca87958f2a744e344af17ca3987589942a567f3f
Ivan Snegirev <ivan.snegirev33@gmail.com>
2020-08-14 01:32:02 +0300

    add entrypoint

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,7 @@
 import os
 from time import sleep
 
-while len([i for i in os.listdir() if '.onnx' in i])==7:
+while len([i for i in os.listdir() if '.onnx' in i])<7:
     sleep(1)
     print('Еще не загрузилось')
     pass
