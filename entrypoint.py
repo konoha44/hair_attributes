0f05518be00281412b63e6afa83b2e64bb839343
Ivan Snegirev <ivan.snegirev33@gmail.com>
2020-08-14 02:02:51 +0300

    fix commit

--- entrypoint.py
+++ entrypoint.py
@@ -2,7 +2,6 @@ import os
 from time import sleep
 
 while len([i for i in os.listdir() if '.onnx' in i])<7:
-    sleep(1)
+    sleep(15)
     pass
-sleep(20)
 print('\n\n все модели загружены из яндекса, можем запускать фласк\n\n')
