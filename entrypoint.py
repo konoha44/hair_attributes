46629588ff68f8a75dfbec5fa1a33efe59288a91
Ivan Snegirev <ivan.snegirev33@gmail.com>
2020-08-14 01:53:40 +0300

    add entrypoint

--- entrypoint.py
+++ entrypoint.py
@@ -4,4 +4,5 @@ from time import sleep
 while len([i for i in os.listdir() if '.onnx' in i])<7:
     sleep(1)
     pass
+sleep(20)
 print('\n\n все модели загружены из яндекса, можем запускать фласк\n\n')
